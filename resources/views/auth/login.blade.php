36145195105ad1a1f8b0fce403761ea7d8e91d8d
Muneeb1Geek <m.muneebulrahman@gmail.com>
2025-05-07 13:13:45 +0500

    login page alert added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,6 +13,24 @@
                 </div>
                 <form action="{{ route('login') }}" method="post">
                     @csrf
+                    @if ($errors->any())
+                        <div class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+                            role="alert">
+                            <svg class="shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                            </svg>
+                            <span class="sr-only">Info</span>
+                            <div>
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li class="font-medium">{{$error}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    @endif
                     <div class="mb-4">
                         <label for="email" class="block text-base text-[#333333]">
                             Login
@@ -52,17 +70,17 @@
                             </label>
                             <span class="py-2 md:py-0"> Remember me</span>
                         </div>
-                        <a href="#" class="forgetPassword text-[#007AFF] text-sm">Forgot Password?</a>
+                        {{-- <a href="#" class="forgetPassword text-[#007AFF] text-sm">Forgot Password?</a> --}}
                     </div>
                     <button type="submit"
                         class="my-7 mb-4 block text-center w-full font-semibold text-[16px] text-black py-3 rounded-3xl signInBtn bg-[#EBC23A] rounded-full">
                         Sign in
-                </button>
+                    </button>
 
-                    <p class="text-center text-sm md:text-base">
+                    {{-- <p class="text-center text-sm md:text-base">
                         Dont have an account?
                         <span class="forgetPassword text-[#007AFF]">Sign up now</span>
-                    </p>
+                    </p> --}}
                 </form>
             </div>
         </div>
