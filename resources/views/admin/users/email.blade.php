0857aa2cc23207d05665fc5df5d67f085e437dd5
= <=>
2025-06-12 15:36:32 +0500

    debugging

--- resources/views/admin/users/email.blade.php
+++ resources/views/admin/users/email.blade.php
@@ -432,7 +432,7 @@
         // actionUrl = actionUrl.replace(':id', emailFormatId);
         // console.log("emailFormatId:", emailFormatId);
         let actionUrl = "{{ url('/users/update/email-format') }}/" + emailFormatId;
-        console.log('here url', actionUrl);
+        console.log('here url', actionUrl, emailFormatId);
 
         fetch(actionUrl, {
                 method: "POST",
