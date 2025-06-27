6fc1eecc7ac38d5865832fcd49d9bfbb8240dc9b
= <=>
2025-06-27 18:34:04 +0500

    UPDATE : fix the issue in the comment section

--- resources/views/admin/users/onboarding_details.blade.php
+++ resources/views/admin/users/onboarding_details.blade.php
@@ -115,20 +115,18 @@
                     <input type="password" name="linkedin_password" id="linkedin_password"
                         class="w-full border border-gray-300 bg-gray-100 rounded-lg p-3 focus:outline-none focus:ring-2 focus:ring-[#F3C941] pr-10" value="{{ $accountDetails->linkedin_password ?? '' }}" readonly />
                     <!-- Eye icon to toggle password visibility -->
-                    <button type="button" id="togglePassword password"
-                        class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none passwordToggle" onclick="togglePassword(this)">
-                        <!-- Eye Icon (Visible state) -->
-                        <svg id="eyeIcon" width="17" height="12" viewBox="0 0 17 12" fill="none"
-                            xmlns="http://www.w3.org/2000/svg" class="hidden">
-                            <path
-                                d="M8.26611 0.153381C4.62975 0.153381 1.5243 2.4152 0.266113 5.60793C1.5243 8.80065 4.62975 11.0625 8.26611 11.0625C11.9025 11.0625 15.0079 8.80065 16.2661 5.60793C15.0079 2.4152 11.9025 0.153381 8.26611 0.153381ZM8.26611 9.24429C6.25884 9.24429 4.62975 7.6152 4.62975 5.60793C4.62975 3.60065 6.25884 1.97156 8.26611 1.97156C10.2734 1.97156 11.9025 3.60065 11.9025 5.60793C11.9025 7.6152 10.2734 9.24429 8.26611 9.24429ZM8.26611 3.42611C7.05884 3.42611 6.0843 4.40065 6.0843 5.60793C6.0843 6.8152 7.05884 7.78974 8.26611 7.78974C9.47339 7.78974 10.4479 6.8152 10.4479 5.60793C10.4479 4.40065 9.47339 3.42611 8.26611 3.42611Z"
-                                fill="#DCD9DE" />
+                    <button type="button"
+                        class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none passwordToggle"
+                        onclick="togglePassword(this)">
+
+                        <!-- Eye icon -->
+                        <svg class="eye-icon block" xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 17 12" fill="none">
+                            <path d="M8.26611 0.153381C4.62975 0.153381 1.5243 2.4152 0.266113 5.60793C1.5243 8.80065 4.62975 11.0625 8.26611 11.0625C11.9025 11.0625 15.0079 8.80065 16.2661 5.60793C15.0079 2.4152 11.9025 0.153381 8.26611 0.153381ZM8.26611 9.24429C6.25884 9.24429 4.62975 7.6152 4.62975 5.60793C4.62975 3.60065 6.25884 1.97156 8.26611 1.97156C10.2734 1.97156 11.9025 3.60065 11.9025 5.60793C11.9025 7.6152 10.2734 9.24429 8.26611 9.24429ZM8.26611 3.42611C7.05884 3.42611 6.0843 4.40065 6.0843 5.60793C6.0843 6.8152 7.05884 7.78974 8.26611 7.78974C9.47339 7.78974 10.4479 6.8152 10.4479 5.60793C10.4479 4.40065 9.47339 3.42611 8.26611 3.42611Z" fill="#DCD9DE" />
                         </svg>
 
-                        <!-- Eye Off Icon (Hidden by default) -->
-
-                        <svg id="eyeOffIcon" width="17" height="17" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.7429 5.09232C11.1494 5.03223 11.5686 5 12.0004 5C17.1054 5 20.4553 9.50484 21.5807 11.2868C21.7169 11.5025 21.785 11.6103 21.8231 11.7767C21.8518 11.9016 21.8517 12.0987 21.8231 12.2236C21.7849 12.3899 21.7164 12.4985 21.5792 12.7156C21.2793 13.1901 20.8222 13.8571 20.2165 14.5805M6.72432 6.71504C4.56225 8.1817 3.09445 10.2194 2.42111 11.2853C2.28428 11.5019 2.21587 11.6102 2.17774 11.7765C2.1491 11.9014 2.14909 12.0984 2.17771 12.2234C2.21583 12.3897 2.28393 12.4975 2.42013 12.7132C3.54554 14.4952 6.89541 19 12.0004 19C14.0588 19 15.8319 18.2676 17.2888 17.2766M3.00042 3L21.0004 21M9.8791 9.87868C9.3362 10.4216 9.00042 11.1716 9.00042 12C9.00042 13.6569 10.3436 15 12.0004 15C12.8288 15 13.5788 14.6642 14.1217 14.1213" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="opacity-[.3]" />
+                        <!-- Eye-off icon -->
+                        <svg class="eye-off-icon hidden" xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 24 24" fill="none">
+                            <path d="M2 2L22 22M17.94 17.94C16.37 19.18 14.31 20 12 20C7 20 2.73 16.11 1 12C1.68 10.3 2.76 8.79 4.1 7.6M10.67 10.67C10.24 11.11 10 11.68 10 12.3C10 13.74 11.26 15 12.7 15C13.32 15 13.89 14.76 14.33 14.33M9.17 4.26C10.08 4.09 11.03 4 12 4C17 4 21.27 7.89 23 12C22.52 13.11 21.86 14.14 21.06 15.06" stroke="#DCD9DE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </button>
                 </div>
@@ -156,6 +154,33 @@
             eyeOffIcon.classList.add('hidden');
         }
     }
+    // $('.passwordToggle').on('click', function() {
+    //     $(this).toggleClass('active');
+    //     var input = $(this).prev('input');
+    //     if (input.attr('type') == 'password') {
+    //         input.attr('type', 'text');
+    //     } else {
+    //         input.attr('type', 'password');
+    //     }
+
+    //     $(this).find('svg').toggleClass('hidden');
+    // });
+
+    // function togglePassword(button) {
+    //     const passwordInput = document.querySelector('input[type="password"], input[type="text"]');
+    //     const eyeIcon = button.querySelector('.eye-icon');
+    //     const eyeOffIcon = button.querySelector('.eye-off-icon');
+
+    //     if (passwordInput.type === "password") {
+    //         passwordInput.type = "text";
+    //         eyeIcon.classList.add('hidden');
+    //         eyeOffIcon.classList.remove('hidden');
+    //     } else {
+    //         passwordInput.type = "password";
+    //         eyeIcon.classList.remove('hidden');
+    //         eyeOffIcon.classList.add('hidden');
+    //     }
+    // }
 </script>
 
 @endpush
--- resources/views/user/onboarding/index.blade.php
+++ resources/views/user/onboarding/index.blade.php
@@ -9,28 +9,6 @@
     </h5>
 
     <main class="w-full max-w-xl mx-auto flex flex-col items-center">
-
-        <!-- Introduction Card -->
-        <div class="bg-white w-full shadow rounded-[26px] py-8 px-5 md:px-20 text-center mb-8">
-            <h1 class="text-[20px] font-bold text-[#1B1B1F]">Lorem ipsum dolor</h1>
-            <p class="text-[14px] leading-22.4 font-normal text-[#46464F] pt-[8px]">
-                Lorem ipsum dolor sit amet consectetur. Arcu ac interdum orci orci
-            </p>
-        </div>
-
-        <!-- Highlight Card -->
-        <div class="bg-white w-full shadow rounded-[26px] py-8 px-5 mb-8 flex flex-col gap-5 md:flex-row items-center justify-between">
-            <div class="flex-1">
-                <h1 class="text-[20px] leading-20 font-bold">Lorem, ipsum dolor</h1>
-                <p class="text-[14px] leading-22.4 text-[#46464F] max-w-[262px]">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, ipsum!
-                </p>
-            </div>
-            <span class="bg-[#F3C941] text-[#000000] text-[16px] font-semibold max-h-[49px] w-[188px] py-3 px-5 text-center rounded-full">
-                The Seels Scan
-            </span>
-        </div>
-
         <!-- Email Box Details -->
         <div class="bg-white w-full shadow rounded-[26px] py-8 px-5 mb-8">
             <form action="{{ route('onboarding.store') }}" method="post">
@@ -43,32 +21,34 @@
 
                 <div id="email-groups-wrapper" class="mt-6">
                     @forelse($accountDetails as $index => $emailDetail)
-                    <!-- <div class="email-group mb-6 border p-4 rounded-lg"> -->
                     <div class="email-group mb-6 border p-4 rounded-lg relative">
+                        <input type="hidden" name="email_ids[]" value="{{ $emailDetail->id }}">
 
-                        <!-- <button type="button"
-                            onclick="submitDelete({{ $emailDetail->id }})"
-                            class="absolute top-2 right-2 text-yellow-600 font-semibold hover:underline text-lg">
-                            X
-                        </button> -->
                         <button type="button"
                             onclick="submitDelete({{ $emailDetail->id }})"
-                            class="absolute top-2 right-2 text-yellow-600 font-semibold text-lg">
-                            X
-                        </button>
+                            class="absolute top-2 right-2 text-yellow-600 font-semibold text-lg">X</button>
 
                         <label class="block text-base text-[#333333] mb-2 mt-4">Email</label>
                         <input type="email" name="email_email[]" class="w-full border border-gray-300 rounded-lg p-3"
                             placeholder="Enter Email" value="{{ $emailDetail->email_email ?? '' }}" required />
 
                         <label class="block text-base text-[#333333] mb-2 mt-4">Password</label>
-
-
                         <div class="relative">
                             <input type="password" name="email_password[]" class="w-full border border-gray-300 rounded-lg p-3 pr-10 password-field"
                                 placeholder="Enter your password" value="{{ $emailDetail->email_password ?? '' }}" required />
-                            <button type="button" class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 password-toggle">
-                                👁
+                            <button type="button"
+                                class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none passwordToggle"
+                                onclick="togglePassword(this)">
+
+                                <!-- Eye icon -->
+                                <svg class="eye-icon block" xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 17 12" fill="none">
+                                    <path d="M8.26611 0.153381C4.62975 0.153381 1.5243 2.4152 0.266113 5.60793C1.5243 8.80065 4.62975 11.0625 8.26611 11.0625C11.9025 11.0625 15.0079 8.80065 16.2661 5.60793C15.0079 2.4152 11.9025 0.153381 8.26611 0.153381ZM8.26611 9.24429C6.25884 9.24429 4.62975 7.6152 4.62975 5.60793C4.62975 3.60065 6.25884 1.97156 8.26611 1.97156C10.2734 1.97156 11.9025 3.60065 11.9025 5.60793C11.9025 7.6152 10.2734 9.24429 8.26611 9.24429ZM8.26611 3.42611C7.05884 3.42611 6.0843 4.40065 6.0843 5.60793C6.0843 6.8152 7.05884 7.78974 8.26611 7.78974C9.47339 7.78974 10.4479 6.8152 10.4479 5.60793C10.4479 4.40065 9.47339 3.42611 8.26611 3.42611Z" fill="#DCD9DE" />
+                                </svg>
+
+                                <!-- Eye-off icon -->
+                                <svg class="eye-off-icon hidden" xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 24 24" fill="none">
+                                    <path d="M2 2L22 22M17.94 17.94C16.37 19.18 14.31 20 12 20C7 20 2.73 16.11 1 12C1.68 10.3 2.76 8.79 4.1 7.6M10.67 10.67C10.24 11.11 10 11.68 10 12.3C10 13.74 11.26 15 12.7 15C13.32 15 13.89 14.76 14.33 14.33M9.17 4.26C10.08 4.09 11.03 4 12 4C17 4 21.27 7.89 23 12C22.52 13.11 21.86 14.14 21.06 15.06" stroke="#DCD9DE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
                             </button>
                         </div>
 
@@ -78,17 +58,17 @@
                     </div>
                     @empty
                     <div class="email-group mb-6 border p-4 rounded-lg">
+                        <input type="hidden" name="email_ids[]" value="">
+
                         <label class="block text-base text-[#333333] mb-2">Email</label>
                         <input type="email" name="email_email[]" class="w-full border border-gray-300 rounded-lg p-3" placeholder="Enter Email" required />
 
                         <label class="block text-base text-[#333333] mb-2 mt-4">Password</label>
                         <div class="relative">
-                            <input type="password" name="email_password[]" class="w-full border border-gray-300 rounded-lg p-3 pr-10 password-field"
-                                placeholder="Enter Password" required />
-                            <button type="button" class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 password-toggle">
-                                👁
-                            </button>
+                            <input type="password" name="email_password[]" class="w-full border border-gray-300 rounded-lg p-3 pr-10 password-field" placeholder="Enter Password" required />
+                            <button type="button" class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 password-toggle">👁</button>
                         </div>
+
                         <label class="block text-base text-[#333333] mb-2 mt-4">Platform</label>
                         <input type="text" name="email_types[]" class="w-full border border-gray-300 rounded-lg p-3" placeholder="Enter Email Type" required />
                     </div>
@@ -105,11 +85,11 @@
 
             </form>
         </div>
+
         <form id="deleteForm" method="POST" style="display: none;">
             @csrf
             @method('DELETE')
         </form>
-
         <!-- LinkedIn Details -->
         <div class="bg-white w-full shadow rounded-[26px] py-8 px-5 mb-8">
             <form action="{{ route('onboarding.store') }}" method="post">
@@ -134,8 +114,19 @@
                         <input type="password" name="linkedin_password" id="linkedin_password"
                             class="w-full border border-gray-300 rounded-lg p-3 pr-10 password-field"
                             placeholder="Enter your password" value="{{ old('linkedin_email', $accountDetail->linkedin_password ?? '') }}" required />
-                        <button type="button" class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 password-toggle">
-                            👁
+                        <button type="button"
+                            class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none passwordToggle"
+                            onclick="togglePassword(this)">
+
+                            <!-- Eye icon -->
+                            <svg class="eye-icon block" xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 17 12" fill="none">
+                                <path d="M8.26611 0.153381C4.62975 0.153381 1.5243 2.4152 0.266113 5.60793C1.5243 8.80065 4.62975 11.0625 8.26611 11.0625C11.9025 11.0625 15.0079 8.80065 16.2661 5.60793C15.0079 2.4152 11.9025 0.153381 8.26611 0.153381ZM8.26611 9.24429C6.25884 9.24429 4.62975 7.6152 4.62975 5.60793C4.62975 3.60065 6.25884 1.97156 8.26611 1.97156C10.2734 1.97156 11.9025 3.60065 11.9025 5.60793C11.9025 7.6152 10.2734 9.24429 8.26611 9.24429ZM8.26611 3.42611C7.05884 3.42611 6.0843 4.40065 6.0843 5.60793C6.0843 6.8152 7.05884 7.78974 8.26611 7.78974C9.47339 7.78974 10.4479 6.8152 10.4479 5.60793C10.4479 4.40065 9.47339 3.42611 8.26611 3.42611Z" fill="#DCD9DE" />
+                            </svg>
+
+                            <!-- Eye-off icon -->
+                            <svg class="eye-off-icon hidden" xmlns="http://www.w3.org/2000/svg" width="17" height="12" viewBox="0 0 24 24" fill="none">
+                                <path d="M2 2L22 22M17.94 17.94C16.37 19.18 14.31 20 12 20C7 20 2.73 16.11 1 12C1.68 10.3 2.76 8.79 4.1 7.6M10.67 10.67C10.24 11.11 10 11.68 10 12.3C10 13.74 11.26 15 12.7 15C13.32 15 13.89 14.76 14.33 14.33M9.17 4.26C10.08 4.09 11.03 4 12 4C17 4 21.27 7.89 23 12C22.52 13.11 21.86 14.14 21.06 15.06" stroke="#DCD9DE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
                         </button>
                     </div>
                 </div>
@@ -152,7 +143,6 @@
 
 @push('script')
 <script>
-    // Toggle password visibility for all buttons
     document.addEventListener('click', function(e) {
         if (e.target.closest('.password-toggle')) {
             const button = e.target.closest('.password-toggle');
@@ -164,37 +154,48 @@
     function submitDelete(id) {
         if (confirm("Are you sure you want to delete this email?")) {
             const form = document.getElementById('deleteForm');
-            form.action = `/email/delete/${id}`; // use route('email.delete', id) if using Laravel named routes in JS
+            form.action = `/email/delete/${id}`;
             form.submit();
         }
     }
-
 
     function addEmailGroup() {
         const wrapper = document.getElementById('email-groups-wrapper');
         const div = document.createElement('div');
         div.className = 'email-group mb-6 border p-4 rounded-lg relative';
         div.innerHTML = `
-      <button type="button"
-         onclick="this.parentNode.remove()"
-        class="absolute top-2 right-2 text-yellow-600 font-semibold text-lg">
-           X
-      </button>
+            <input type="hidden" name="email_ids[]" value="">
+            <button type="button" onclick="this.parentNode.remove()" class="absolute top-2 right-2 text-yellow-600 font-semibold text-lg">X</button>
 
+            <label class="block text-base text-[#333333] mb-2 mt-4">Email</label>
+            <input type="email" name="email_email[]" class="w-full border border-gray-300 rounded-lg p-3" placeholder="Enter your Email" required />
 
-        <label class="block text-base text-[#333333] mb-2 mt-4">Email</label>
-        <input type="email" name="email_email[]" class="w-full border border-gray-300 rounded-lg p-3" placeholder="Enter your Email" required />
+            <label class="block text-base text-[#333333] mb-2 mt-4">Password</label>
+            <div class="relative">
+                <input type="password" name="email_password[]" class="w-full border border-gray-300 rounded-lg p-3 pr-10 password-field" placeholder="Enter your password" required />
+                <button type="button" class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 password-toggle">👁</button>
+            </div>
 
-        <label class="block text-base text-[#333333] mb-2 mt-4">Password</label>
-        <div class="relative">
-            <input type="password" name="email_password[]" class="w-full border border-gray-300 rounded-lg p-3 pr-10 password-field" placeholder="Enter your password" required />
-            <button type="button" class="absolute top-1/2 right-3 transform -translate-y-1/2 text-gray-500 password-toggle">👁</button>
-        </div>
-        
-        <label class="block text-base text-[#333333] mb-2 mt-4">Platform</label>
-        <input type="text" name="email_types[]" class="w-full border border-gray-300 rounded-lg p-3" placeholder="Enter Email Type" required />
-
-    `;
+            <label class="block text-base text-[#333333] mb-2 mt-4">Platform</label>
+            <input type="text" name="email_types[]" class="w-full border border-gray-300 rounded-lg p-3" placeholder="Enter Email Type" required />
+        `;
         wrapper.appendChild(div);
     }
+
+    function togglePassword(button) {
+        const input = button.closest('.relative').querySelector('input');
+        const eyeIcon = button.querySelector('.eye-icon');
+        const eyeOffIcon = button.querySelector('.eye-off-icon');
+
+        if (input.type === "password") {
+            input.type = "text";
+            eyeIcon.classList.add('hidden');
+            eyeOffIcon.classList.remove('hidden');
+        } else {
+            input.type = "password";
+            eyeIcon.classList.remove('hidden');
+            eyeOffIcon.classList.add('hidden');
+        }
+    }
 </script>
+@endpush
